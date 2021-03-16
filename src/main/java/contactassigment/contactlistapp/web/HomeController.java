690c78a40a882b36e07fca6a7236526572c85bdf
ckarimajji <kchaitanya.mail@gmail.com>
2021-03-16 12:22:17 +1030

    to show username from env

--- src/main/java/contactassigment/contactlistapp/web/HomeController.java
+++ src/main/java/contactassigment/contactlistapp/web/HomeController.java
@@ -1,16 +1,21 @@
 package contactassigment.contactlistapp.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class HomeController
-{
+public class HomeController {
+	
+	@Autowired
+	Environment env;
 
-  @RequestMapping("/")
-  public String home()
-  {
-    return "/home";
-  }
+	@RequestMapping("/")
+	public String home(Model model) {
+		model.addAttribute("username", env.getProperty("user.name"));
+		return "/home";
+	}
 
 }
--- src/main/webapp/WEB-INF/jsp/home.jsp
+++ src/main/webapp/WEB-INF/jsp/home.jsp
@@ -1,10 +1,11 @@
 <%@ page pageEncoding="UTF-8"%>
 <%@ taglib prefix="t" tagdir="/WEB-INF/tags" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
 <t:layout title="Home" context="${pageContext.servletContext.contextPath}">
     <jsp:body>
         <div class="row">
             <div class="col-md-12" style="text-align: center">
-                <h4>Welcome give user name!<h4>
+                <h4>Welcome <c:out value="${username}"/>!<h4>
             </div>
         </div>
     </jsp:body>
